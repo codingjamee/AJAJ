cd34032e261650b9672c2516d7d63eb89e45d560
[AI9] 장재웅 <wodndrhdqn@gmail.com>
2023-10-27 19:26:05 +0000

    [봉준]Fixed: 로그아웃 후에도  홈 화면 렌더링 되는 오류 수정

--- front/src/components/utils/axiosConfig.js
+++ front/src/components/utils/axiosConfig.js
@@ -6,7 +6,7 @@ import axios from "axios";
 //request사용
 const config = {
   baseURL:
-  process.env.REACT_APP_API_BASE_URL,// process.env.REACT_APP_BUILD_BASE_URL,
+  process.env.REACT_APP_API_BASE_URL ||  process.env.REACT_APP_BUILD_BASE_URL,
   headers: { "Content-Type": "application/json" },
   timeout: 5000,
   withCredentials: true,
