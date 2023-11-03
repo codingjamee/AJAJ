15774c96ab28974192481778a343abbdba612295
[AI9] 최유림 <ul051101@gmail.com>
2023-11-03 21:49:13 +0900

    [유림] Fix: 경로수정

--- front/src/App.js
+++ front/src/App.js
@@ -8,7 +8,7 @@ import React, {
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 
 import * as Api from "./utils/api";
-import { loginReducer } from "./components/reducers/loginReducer";
+import { loginReducer } from "./reducers/loginReducer";
 
 import Navigation from "./components/common/header/Navigation";
 import Login from "./components/pages/login/Login";
