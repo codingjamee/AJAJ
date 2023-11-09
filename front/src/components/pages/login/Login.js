c4e4cd04ea02f4c65301351d4b630e103b04104c
[AI9] 허제인 <codingjamee@gmail.com>
2023-11-09 09:53:00 +0900

    [제인] Feature: resetErrorBoundary 버튼 추가, suspense 넣어보기, 포괄적 에러바운더리 넣어보기

--- front/src/components/pages/login/Login.js
+++ front/src/components/pages/login/Login.js
@@ -1,20 +1,36 @@
 import { ErrorBoundary } from "react-error-boundary";
 import LoginForm from "./LoginForm";
 import img from "../../../components/common/header/logo0.png";
+import ButtonCommon from "../../common/ButtonCommon";
+
+const BtnSetting = {
+  variant: "primary",
+  type: "submit",
+  className: "me-3",
+  text: "로그인 다시하러가기",
+};
+
+const ErrorFallback = ({ resetErrorBoundary }) => {
+  return (
+    <div>
+      <h1>
+        로그인에 실패했습니다
+        <img src={img} alt="err" />
+        ...
+      </h1>
+      <ButtonCommon
+        {...BtnSetting}
+        onClickHandler={() => {
+          resetErrorBoundary();
+        }}
+      />
+    </div>
+  );
+};
 
 const Login = () => {
   return (
-    <ErrorBoundary
-      fallback={
-        <>
-          <h1>
-            로그인에 실패했습니다
-            <img src={img} alt="err" />
-            ...
-          </h1>
-        </>
-      }
-    >
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
       <LoginForm />
     </ErrorBoundary>
   );
